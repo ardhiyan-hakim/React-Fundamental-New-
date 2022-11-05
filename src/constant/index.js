ccf343d7aed5e3f68aa5707b4cfc6769e3f585ce
Ardhiyan Abdul Hakim <ardhiyan.hakim.bsn@gmail.com>
2022-11-05 18:05:21 +0700

    Add Constant Variable

--- src/constant/index.js
+++ src/constant/index.js
@@ -1,6 +1,8 @@
-export const DEFAULT_PATH = "/React-Fundamental/"
-export const ARCHIVE_PAGE_PATH = "/React-Fundamental/archives/"
-export const ADD_PAGE_PATH = "/React-Fundamental/add/"
-export const DETAIL_PAGE_PATH = "/React-Fundamental/detail/:id"
-export const LANDING_PAGE_PATH = "/React-Fundamental/login"
-export const NOT_FOUND_PATH = "/React-Fundamental/*"
+const BASE_URL = "/React-Notes";
+
+export const DEFAULT_PATH = `${BASE_URL}`;
+export const ARCHIVE_PAGE_PATH = `${BASE_URL}/archives`;
+export const ADD_PAGE_PATH = `${BASE_URL}/add`;
+export const DETAIL_PAGE_PATH = `${BASE_URL}detail/:id`;
+export const LANDING_PAGE_PATH = `${BASE_URL}/login`;
+export const NOT_FOUND_PATH = `${BASE_URL}/*`;
